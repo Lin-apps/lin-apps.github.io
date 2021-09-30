5f91e4242fe09f95cd8eae91ddd0c3febc8a0ae2
Lin-apps <68005465+Lin-apps@users.noreply.github.com>
2021-09-30 16:05:20 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,12 @@
-if(document.getElementById('button').clicked == true)
-{
-   alert("button was clicked");
-}
+var url = 'https://newsapi.org/v2/everything?' +
+          'q=Apple&' +
+          'from=2021-09-30&' +
+          'sortBy=popularity&' +
+          'apiKey=4f4388190e044f4d80e0a27e7cbaf70b';
+
+var req = new Request(url);
+
+fetch(req)
+    .then(function(response) {
+        console.log(response.json());
+    })
