fc14f27f9855d1a4ba0bed66aa9445205b3aad0e
Lin-apps <68005465+Lin-apps@users.noreply.github.com>
2021-10-09 22:24:17 +0200

    s

--- test.js
+++ test.js
@@ -1,16 +1,50 @@
-import { getDatabase, ref, onValue} from "firebase/database";
+function Comments(fbname) {
 
-const db = getDatabase();
-const starCountRef = ref(db, 'posts/' + postId + '/starCount');
-onValue(starCountRef, (snapshot) => {
-  const data = snapshot.val();
-  updateStarCount(postElement, data);
+  var firebase = new Firebase("https://" + fbname + ".firebaseio.com/");
+  this.firebase = firebase;
+  var commentsRef = firebase.child('comments');
+
+  this.submitComment = function(text) {
+    commentsRef.push({
+    text: text
+    });
+  };
+
+  this.onCommentsChanged = function() {};
+
+ commentsRef.on('value', function(snapshot) {
+    var comments = snapshot.val();
+    var preparedComments = [];
+	
+    for (var text in comments) {
+      if (comments.hasOwnProperty(text)) {
+        preparedComments.push({
+          text: comments.text
+        })
+      }
+    }
+    this.onCommentsChanged(preparedComments);
+  }.bind(this));
+ 
+};
+
+$(document).ready(function() {
+
+	var ll = new Comments('fbname');
+
+	$(".comment-form form").submit(function(event) {
+    event.preventDefault();
+    ll.submitComment($(this).find('input.comment-text').val());
+    $(this).find("input[type=text]").val("").blur();
+    return false;
+  });
+
+  ll.onCommentsChanged = function(comments) {
+    $(".comments-list").empty();
+    comments.map(function(comment) {
+      var commentElement = "<li><a href='" + comments.text + "</a></li>";
+      $(".comments-list").append(commentElement);
+    });
+  };
+
 });
-
-alert(data);
-
-document.getElementById('button').onclick = function() {
-   alert(req);
-}
-
-
