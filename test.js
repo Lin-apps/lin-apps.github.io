cbd7516b034c9b79dc9425e2eb5774cc1ce8c8a1
Lin-apps <68005465+Lin-apps@users.noreply.github.com>
2021-10-09 22:26:58 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,50 +1,6 @@
-function Comments(fbname) {
-
-  var firebase = new Firebase("https://" + fbname + ".firebaseio.com/");
-  this.firebase = firebase;
-  var commentsRef = firebase.child('comments');
-
-  this.submitComment = function(text) {
-    commentsRef.push({
-    text: text
-    });
-  };
-
-  this.onCommentsChanged = function() {};
-
- commentsRef.on('value', function(snapshot) {
-    var comments = snapshot.val();
-    var preparedComments = [];
-	
-    for (var text in comments) {
-      if (comments.hasOwnProperty(text)) {
-        preparedComments.push({
-          text: comments.text
-        })
-      }
-    }
-    this.onCommentsChanged(preparedComments);
-  }.bind(this));
- 
-};
-
-$(document).ready(function() {
-
-	var ll = new Comments('fbname');
-
-	$(".comment-form form").submit(function(event) {
-    event.preventDefault();
-    ll.submitComment($(this).find('input.comment-text').val());
-    $(this).find("input[type=text]").val("").blur();
-    return false;
+var rootRef = firebase.database().ref();
+rootRef.once("value")
+  .then(function(snapshot) {
+    var key = snapshot.key; // null
+    var childKey = snapshot.child("users/ada").key; // "ada"
   });
-
-  ll.onCommentsChanged = function(comments) {
-    $(".comments-list").empty();
-    comments.map(function(comment) {
-      var commentElement = "<li><a href='" + comments.text + "</a></li>";
-      $(".comments-list").append(commentElement);
-    });
-  };
-
-});
